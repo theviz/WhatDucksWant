80c1e3cc3d5c2cf65d33d9cc2ded5bb1a37228f3
Viz <vmuthukumaraswamy@rezilyens.com>
2021-09-27 00:46:47 -0300

    [#Refactoring] Added method to create food type and item when they don't exist
    Restored the URLs to the original value
    
    Cleaned up unwanted print statements
    
    Created Methods to extract id
    
    Changed to schema and api

--- app/schema.py
+++ app/schema.py
@@ -1,11 +1,3 @@
-# ASSUMPTIONS:
-# ID, TITLE, CONTENT are considered as mandatory fields.
-# The patterns are derived from the model input.
-# Patterns:
-#   id => Start with a character and followed by characters or numbers
-#   type => characters, numbers, hyphens, underbars, and space.
-#   description => characters, special characters, and space.
-#   Views and timestamp should be greater than 0
 class Schema:
     def __init__(self):
         self.food_type = {
@@ -35,10 +27,15 @@ class Schema:
             },
             "required": ["itemname", "food_id"]
         }
-        self.duck_food = {
+        self.duck_feed = {
             "type": "object",
             "properties": {
-                "item_id": {
+                "food_type": {
+                    "type": "string",
+                    "minLength": 2,
+                    "pattern": "^[A-Za-z0-9-_]*$"
+                },
+                "item_name": {
                     "type": "string",
                     "minLength": 2,
                     "pattern": "^[A-Za-z0-9-_]*$"
@@ -56,16 +53,15 @@ class Schema:
                     "type": "number",
                     "minimum": 0,
                 },
-                "lat": {
+                "quantity": {
                     "type": "number",
-                    "minimum": -90,
-                    "maximum": 90
+                    "minimum": 0,
                 },
-                "lng": {
-                    "type": "number",
-                    "minimum": -180,
-                    "maximum": 180
+                "location": {
+                    "type": "string",
+                    "minLength": 2,
+                    "pattern": "^[A-Za-z0-9-_\s]*$"
                 },
             },
-            "required": ["item_id", "count", "lat", "lng"]
+            "required": ["item_id", "count", "location", "food_type", "item_name"]
         }
--- app/util.py
+++ app/util.py
@@ -10,28 +10,31 @@ class Util:
         self.config = Config()
 
     def handle_result(self, result):
-        return {'id' : str(result.id)}, self.config.HTTP_CREATED
+        return {'id' : str(result.id)}
 
     def handle_exception(self, exception):
         print(exception, flush = True)
-        return {'error' : str(exception)}, self.config.SERVER_ERROR
+        return {'error' : str(exception)}
 
     def create_food_type(self, data):
         try:
-            foodtype = FoodType(name = data['foodname'])
+            foodtype = FoodType(name = data['food_type'])
             result = foodtype.save()
             return self.handle_result(result)
         except Exception as e:
             print(e)
             raise ValueError('Error while creating food type')
 
+    def get_food_type(self, foodtype):
+        return FoodType.objects(name = foodtype).to_json()
+
     def get_food_types(self):
-        return FoodType.objects().to_json(), self.config.HTTP_CREATED
+        return FoodType.objects().to_json()
 
     def create_food_item(self, data):
         try:
             item = FoodItem(
-                item_name = data['itemname'],
+                item_name = data['item_name'],
                 food_id = data['food_id']
             )
             result = item.save()
@@ -40,8 +43,31 @@ class Util:
             print(e)
             raise ValueError('Error while creating food item')
 
+    def get_food_item(self, itemname):
+        return FoodItem.objects(item_name = itemname).to_json()
+
     def get_food_items(self):
-        return FoodItem.objects().to_json(), self.config.HTTP_CREATED
+        return FoodItem.objects().to_json()
+
+    def get_first_id(self, data):
+        return data[0]['_id']['$oid']
+
+    def get_item_id_for_data(self, data):
+        food_item = json.loads(self.get_food_item(data['item_name']))
+        item_id = None
+
+        if len(food_item) != 0:
+            item_id = self.get_first_id(food_item)
+        else:
+            food_type = json.loads(self.get_food_type(data['food_type']))
+            type_id = None
+            if len(food_type) != 0:
+                type_id = self.get_first_id(food_type)
+            else:
+                type_id = self.create_food_type(data)['id']
+            data['food_id'] = type_id
+            item_id = self.create_food_item(data)['id']
+        return item_id
 
     def create_duck_food(self, data):
         try:
@@ -49,16 +75,16 @@ class Util:
             date = None
             if at != None:
                 date =  dt.fromtimestamp(int(at))
-
+            item_id = self.get_item_id_for_data(data)
+            print(item_id)
             park = DuckPark(
-                item_id = data['item_id'],
+                item_id = item_id,
                 name = data['name'],
                 count = data['count'],
                 at = date,
-                lat = data['lat'],
-                lng = data['lng']
+                location = data['location'],
+                food_amount = data['food_amount']
             )
-
             result = park.save()
             return self.handle_result(result)
 
